11b6c10806c20e567ea74a4a6fd26ade663d81c3
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2021-11-30 17:19:27 +0100

    opkuis NaampadObject.py

--- OTLClasses/Verification/NaampadObject.py
+++ OTLClasses/Verification/NaampadObject.py
@@ -11,8 +11,6 @@ class OTLAsset:
 class NaampadObject(OTLAsset, ABC):
     uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#NaampadObject'
 
-#    def AddRelatie(self, object):
-#        pass
     @abstractmethod
     def __init__(self):
         raise TypeError("Can't instantiate abstract class " + self.__class__.__name__ )

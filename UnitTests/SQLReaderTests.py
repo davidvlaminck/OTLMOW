1fac1d735831f66970339f07fbcbcd84a8afec81
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2021-11-30 01:07:03 +0100

    getclassbyuri

--- UnitTests/SQLReaderTests.py
+++ UnitTests/SQLReaderTests.py
@@ -1,42 +1,33 @@
-import os
-import sqlite3
 import unittest
 
 from ModelGenerator.OSLOClass import OSLOClass
-
-
-class SQLReader:
-    def __init__(self, path):
-        self.path = path
-
-    def getClasses(self):
-        if not(os.path.isfile(self.path)):
-            raise FileNotFoundError()
-
-        con = sqlite3.connect(self.path)
-        cur = con.cursor()
-        list = []
-        for row in cur.execute(
-                "SELECT label_nl, name, uri, definition_nl, usagenote_nl, abstract, deprecated_version FROM OSLOClass where uri=:uriclass",
-                {"uriclass": 'https://wegenenverkeer.data.vlaanderen.be/ns/abstracten#AansluitendeConstructie'}):
-            c = OSLOClass(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
-            list.append(c)
-        con.close()
-        return list
+from ModelGenerator.SQLReader import SQLReader
 
 
 class SQLReaderTests(unittest.TestCase):
     def test_FileNotFound(self):
         fileLocation = ''
         sqlReader = SQLReader(fileLocation)
-        self.assertRaises(FileNotFoundError, sqlReader.getClasses)
+        self.assertRaises(FileNotFoundError, sqlReader.getAllClasses)
 
-    def test_GetAtLeastOneOSLOClass(self):
+    def test_GetAtLeastOneOSLOClassWithGetAllClasses(self):
         fileLocation = '../InputFiles/OTL.db'
         sqlReader = SQLReader(fileLocation)
-        listOfClasses = sqlReader.getClasses()
+        listOfClasses = sqlReader.getAllClasses()
 
         self.assertTrue(len(listOfClasses) > 0)
         first = next(c for c in listOfClasses)
         self.assertEqual(type(first), OSLOClass)
+
+    def test_GetOneOSLOClassWithgetClassByUri(self):
+        fileLocation = '../InputFiles/OTL.db'
+        sqlReader = SQLReader(fileLocation)
+        uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/abstracten#AansluitendeConstructie'
+        listOfClasses = sqlReader.getClassByUri(uri)
+
+        self.assertTrue(len(listOfClasses) == 1)
+        first = next(c for c in listOfClasses)
+        self.assertEqual(type(first), OSLOClass)
+        self.assertEqual(first.uri, uri)
+
 

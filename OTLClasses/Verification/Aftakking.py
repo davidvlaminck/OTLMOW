1f212a9dd1cac74125143370a8126332a97fa752
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2021-12-08 17:22:23 +0100

    verduidelijken TODO

--- OTLClasses/Verification/Aftakking.py
+++ OTLClasses/Verification/Aftakking.py
@@ -6,6 +6,6 @@ class Aftakking(AIMNaamObject):
     er standaard 4 aftakkingen voorzien: middenberm (hoofdrijbaan),op- en afritten + signalisatie,gewestweg,parking. """
 
     def __init__(self):
-        self.loadGeldigeRelaties()  # TODO move to Factory?
+        self.loadGeldigeRelaties()  # TODO move to Factory? alleen toe te passen op concrete classes
 
     typeURI = "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Aftakking"

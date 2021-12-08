60a19399ae55b63599208ba96acdab6db9baed28
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2021-12-08 17:20:58 +0100

    aanpassen attributen op energiemeter

--- OTLClasses/Verification/Energiemeter.py
+++ OTLClasses/Verification/Energiemeter.py
@@ -16,9 +16,15 @@ class Energiemeter(AIMObject):
     typeUri = "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Energiemeter"
     """De URI van het object volgens https://www.w3.org/2001/XMLSchema#anyURI."""
 
-    antalTelwerken = IntField()
+    antalTelwerken = IntField(naam="aantalTelwerken", label="aantal telwerken",
+                            uri="https://wegenenverkeer.data.vlaanderen.be/ns/abstracten#Energiemeter.aantalTelwerken",
+                            definition="Het aantal telwerken dat de energiemeter bevat: 1 bij enkelvoudige meter, 2 bij een dag- en nacht-meter."
+                            , constraints="", usagenote="", deprecated_version="")
     """Het aantal telwerken dat de energiemeter bevat: 1 bij enkelvoudige meter, 2 bij een dag- en nacht-meter."""
-    meternummer = StringField()
+    meternummer = StringField(naam="meternummer", label="meternummer",
+                            uri="https://wegenenverkeer.data.vlaanderen.be/ns/abstracten#Energiemeter.meternummer",
+                            definition="Het serienummer (nummer van het fabrikaat) op de meter."
+                            , constraints="", usagenote="", deprecated_version="")
     """Het serienummer (nummer van het fabrikaat) op de meter."""
-    metertype = KeuzelijstField(KlEnergiemeterMetertype())
+    metertype = KlEnergiemeterMetertype()
     """Type meter (mechanisch, elektronisch)."""

6ed4a988d4a362a33291c335ad33327dd5688f22
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-01-06 12:58:11 +0100

    UnionTypeField.py implementation fixed

--- OTLModel/Datatypes/UnionTypeField.py
+++ OTLModel/Datatypes/UnionTypeField.py
@@ -7,6 +7,12 @@ class UnionTypeField(OTLField):
         super().__init__(naam=naam, label=label, uri=uri, definition=definition, constraints=None, usagenote=usagenote,
                          deprecated_version=deprecated_version, readonly=readonly, readonlyValue=readonlyValue)
         self.fieldsTuple = fieldsTuple
+        self.actiefVeld = None
+
+    def __getattribute__(self, name):
+        if name == "waarde":
+            return self.actiefVeld.waarde
+        return object.__getattribute__(self, name)
 
     def __setattr__(self, name, value):
         if name == "waarde" and self.readonly and value is not None:
@@ -15,15 +21,26 @@ class UnionTypeField(OTLField):
             if value is None:
                 self.__dict__["waarde"] = value
                 return
-            correcttype = self.check_if_field_in_list(value)
-            if not correcttype:
-                raise TypeError(f'expecting correct type in {self.naam}. Correct types are of type {self.fieldsTuple}')
-            self.__dict__["waarde"] = value
+            if self.actiefVeld is None:
+                raise RuntimeError('Use the method use_field() first to notify what field of the union type you want to use.')
+            if not isinstance(value, type(self.actiefVeld)):
+                raise ValueError(f'{value} is not of the correct type, expecting type of {type(self.actiefVeld)})')
+            self.actiefVeld = value
             return
         self.__dict__[name] = value
 
-    def check_if_field_in_list(self, value):
-        for field in self.fieldsTuple:
-            if isinstance(value, field):
-                return True
-        return False
+    def use_field(self, fieldName):
+        try:
+            self.actiefVeld = next(f for f in self.fieldsTuple if f.naam == fieldName)
+            self.waarde = self.actiefVeld.waarde
+        except StopIteration:
+            msg = f'{fieldName} is not a valid field name for this field. Use of the of the following instead: '
+            for field in self.fieldsTuple:
+                msg += field.naam + ', '
+            msg = msg[:-1]
+            raise ValueError(msg)
+
+    def default(self):
+        if self.actiefVeld is not None:
+            return self.actiefVeld.waarde
+        return None
--- UnitTests/OTLFieldTests/UnionTypeFieldTests.py
+++ UnitTests/OTLFieldTests/UnionTypeFieldTests.py
@@ -1,33 +1,90 @@
 import unittest
 
+from OTLModel.Datatypes.KeuzelijstField import KeuzelijstField
+from OTLModel.Datatypes.KlLichtmastMasthoogte import KlLichtmastMasthoogte
+from OTLModel.Datatypes.KwantWrdInMeter import KwantWrdInMeter
 from OTLModel.Datatypes.UnionTypeField import UnionTypeField
 from OTLModel.Classes.Aftakking import Aftakking
-from OTLModel.Datatypes.DtcAdres import DtcAdres
 from OTLModel.Datatypes.KlAlgGemeente import KlAlgGemeente
 
 
 class UnionTestClass(Aftakking):
-    unionveld = UnionTypeField(naam="testAdres", label="testAdres",
-                               uri="https://wegenenverkeer.data.vlaanderen.be/ns/abstracten#testAdres",
-                               definition="testAdres definitie",
-                               usagenote="", deprecated_version="", fieldsTuple=(DtcAdres, KlAlgGemeente))
+    def __init__(self):
+        super().__init__()
+
+        field1 = KwantWrdInMeter()
+        """De afwijkende hoogte van de mast in meter."""
+        field1.naam = "afwijkendeHoogte"
+        field1.label = "afwijkende hoogte"
+        field1.uri = "https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#DtuLichtmastMasthoogte.afwijkendeHoogte"
+        field1.definition = "De afwijkende hoogte van de mast in meter."
+        field1.constraints = ""
+        field1.usagenote = ""
+        field1.deprecated_version = ""
+
+        field2 = KeuzelijstField(naam="standaardHoogte",
+                                 label="standaard hoogte",
+                                 lijst=KlLichtmastMasthoogte(),
+                                 uri="https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#DtuLichtmastMasthoogte.standaardHoogte",
+                                 definition="Bepaling van de standaard hoogte van een mast.",
+                                 constraints="",
+                                 usagenote="",
+                                 deprecated_version="")
+
+        self.unionveld = UnionTypeField(naam="testUnion",
+                                        label="testUnion",
+                                        uri="https://wegenenverkeer.data.vlaanderen.be/ns/abstracten#testUnion",
+                                        definition="testUnion definitie",
+                                        usagenote="",
+                                        deprecated_version="",
+                                        fieldsTuple=(field1, field2))
 
 
 class KeuzelijstFieldTests(unittest.TestCase):
-    def test_InitField(self):
+    def test_InitClassWithField(self):
         c = UnionTestClass()
+        self.assertIsNotNone(c)
 
-        adresveld = DtcAdres()
-        adresveld.bus.waarde = "BUS"
-        c.unionveld.waarde = adresveld
+    def test_UseFieldInvalidFieldName(self):
+        c = UnionTestClass()
+        with self.assertRaises(ValueError):
+            c.unionveld.use_field('badFieldName')
 
-        self.assertTrue(c.unionveld.waarde.bus.waarde == "BUS")
+    def test_UseField1(self):
+        c = UnionTestClass()
+        c.unionveld.use_field('afwijkendeHoogte')
+        self.assertEqual(c.unionveld.actiefVeld, c.unionveld.fieldsTuple[0])
 
-        gemeenteveld = KlAlgGemeente()
-        gemeenteveld.set_value_by_label('aalst')
-        c.unionveld.waarde = gemeenteveld
+    def test_AssignBeforeUseField(self):
+        c = UnionTestClass()
+        with self.assertRaises(RuntimeError):
+            c.unionveld.waarde = KwantWrdInMeter(7.5)
 
-        self.assertTrue(c.unionveld.waarde.waarde.invulwaarde == "aalst")
+    def test_AssignField1BadValue(self):
+        c = UnionTestClass()
+        c.unionveld.use_field('afwijkendeHoogte')
 
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             c.unionveld.waarde = "2"
+
+    def test_AssignField1(self):
+        c = UnionTestClass()
+        c.unionveld.use_field('afwijkendeHoogte')
+        c.unionveld.waarde = KwantWrdInMeter(7.5)
+        w = c.unionveld.waarde
+        self.assertEqual(7.5, c.unionveld.waarde)
+
+    def test_AssignField2(self):
+        c = UnionTestClass()
+        c.unionveld.use_field('standaardHoogte')
+        c.unionveld.actiefVeld.set_value_by_invulwaarde('10.00')
+        self.assertEqual("10.00", c.unionveld.waarde.invulwaarde)
+
+    def test_AssignField2BadValue(self):
+        c = UnionTestClass()
+        c.unionveld.use_field('standaardHoogte')
+        with self.assertRaises(ValueError):
+            c.unionveld.actiefVeld.set_value_by_invulwaarde("2")
+
+
+

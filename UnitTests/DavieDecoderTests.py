8a260cc5b6576e7de9d42dca60ba1920ac08d2bf
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-01-11 16:33:22 +0100

    DavieDecoder step 1

---
+++ UnitTests/DavieDecoderTests.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from EMInfraResponseTestDouble import ResponseTestDouble
+from Facility.EMInfraDecoder import EMInfraDecoder
+from ModelGenerator.OtlAssetJSONEncoder import OtlAssetJSONEncoder
+from OTLModel.Classes.Omvormer import Omvormer
+
+
+class DavieDecoderTests(TestCase):
+    def test_decodeFirstEntry(self):
+        pass
+

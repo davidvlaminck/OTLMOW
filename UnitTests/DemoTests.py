a388889938656f25166b2b6679e657ae3d384eb9
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-01-18 23:09:14 +0100

    Demo

---
+++ UnitTests/DemoTests.py
@@ -0,0 +1,13 @@
+﻿from unittest import TestCase
+
+from Facility.OTLFacility import OTLFacility
+from Loggers.NoneLogger import NoneLogger
+
+class DemoTests(TestCase):
+    def test_demo(self):
+        fac = OTLFacility(NoneLogger())
+        kring = fac.asset_factory.dynamic_create_instance_from_name('Stroomkring')
+        uri = kring.typeURI
+        assetId = kring.assetId
+        print(kring.attr_info('assetId'))
+        print(kring.attr_type_info('assetId'))

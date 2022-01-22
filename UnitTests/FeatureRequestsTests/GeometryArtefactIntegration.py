add16890868f8d8a823f4fe338367dd838e14653
davidvlaminck <davidvlaminck85@gmail.com>
2022-01-22 07:56:50 +0100

    feature request geometry artefact

---
+++ UnitTests/FeatureRequestsTests/GeometryArtefactIntegration.py
@@ -0,0 +1,18 @@
+import unittest
+from unittest import TestCase
+
+from OTLModel.Classes.Wegkantkast import Wegkantkast
+
+
+class WrongGeometryError(Exception):
+    pass
+
+
+@unittest.skip("feature request")
+class GeometryArtefactIntegration(TestCase):
+    def test_invalid_geometry_based_on_geometry_artefact(self):
+        w = Wegkantkast()
+        with self.assertRaises(WrongGeometryError) as wrong_geometry_exception:
+            w.geometry = 'LINESTRING Z (100000 200000 10, 300000 400000 20)'
+        error_msg = "Asset type Wegkantkast can't be assigned a line as geometry, valid types are point and polygon"
+        self.assertEqual(error_msg, str(wrong_geometry_exception.exception))

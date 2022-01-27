f93c5154a8ee9761ed007473624be8ed64c9de48
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-01-27 22:23:15 +0100

    new release

---
+++ src/OTLMOW/SampleCode/create_model.py
@@ -0,0 +1,17 @@
+from OTLMOW.Facility.OTLFacility import OTLFacility
+from OTLMOW.Loggers.ConsoleLogger import ConsoleLogger
+from OTLMOW.Loggers.LoggerCollection import LoggerCollection
+from OTLMOW.Loggers.TxtLogger import TxtLogger
+
+if __name__ == '__main__':
+    # create the main facade class: OTLFacility
+    logger = LoggerCollection([
+        TxtLogger(r'C:\temp\pythonLogging\pythonlog.txt'),
+        ConsoleLogger()])
+    otl_facility = OTLFacility(logger)
+
+    # create a datamodel based on the OTL SQLite database and ttl files stored on the github
+    otl_file_location = '../InputFiles/OTL.db'
+    otl_facility.init_otl_model_creator(otl_file_location)
+    otl_facility.create_otl_datamodel()
+

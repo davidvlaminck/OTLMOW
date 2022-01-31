b6bcf05c063790c9466cc5b55532f33573b3d748
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-01-31 11:14:08 +0100

    cleanup

--- src/OTLMOW/SampleCode/DemoPresentatie.py
+++ src/OTLMOW/SampleCode/DemoPresentatie.py
@@ -1,11 +1,6 @@
-﻿from OTLMOW.OTLModel.Classes.Stroomkring import Stroomkring
-
-if __name__ == '__main__':
+﻿if __name__ == '__main__':
     print('')
     print('hello world')
-
-    s = Stroomkring()
-    print(s.info_attr(''))
 
 
 

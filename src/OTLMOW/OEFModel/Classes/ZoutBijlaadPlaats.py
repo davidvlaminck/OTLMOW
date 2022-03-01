782c111f10a2a77ed93ac0ead4b560854f00b479
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-03-01 10:08:48 +0100

    updates on enumarations OTL 2.2

---
+++ src/OTLMOW/OEFModel/Classes/ZoutBijlaadPlaats.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from OTLMOW.OEFModel.EMObject import EMObject
+
+
+# Generated with OEFClassCreator. To modify: extend, do not edit
+class ZoutBijlaadPlaats(EMObject):
+    """Bewaakt terrein om zout en pekel te stockeren en te laden."""
+
+    typeURI = 'https://lgc.data.wegenenverkeer.be/ns/installatie#ZoutBijlaadPlaats'
+    label = 'ZBP'
+
+    def __init__(self):
+        super().__init__()
+

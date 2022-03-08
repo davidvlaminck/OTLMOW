9586ba253246c13c10aa0c1e3c036b73f3d90b6d
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-03-08 23:52:27 +0100

    small fixes

--- src/OTLMOW/OTLModel/ClassLoader.py
+++ src/OTLMOW/OTLModel/ClassLoader.py
@@ -1,7 +1,17 @@
-﻿class ClassLoader:
-    """This class attempts to load an OTL class from the corresponding module."""
+﻿from OTLMOW.OTLModel.BaseClasses.OTLObject import OTLObject
+
+
+class ClassLoader:
+    """This class attempts to load an OTL class from the corresponding module and instantiate it."""
     @staticmethod
-    def dynamic_create_instance_from_name(class_name):
+    def dynamic_create_instance_from_name(class_name: str) -> OTLObject | None:
+        """Loads the OTL class module and attempts to instantiate the class using the name of the class
+
+        :param class_name: class name to instantiate
+        :type: str
+        :rtype: OTLObject or None
+        :return: returns an instance of class_name, that inherits from OTLObject
+        """
         try:
             py_mod = __import__(name=f'OTLMOW.OTLModel.Classes.{class_name}', fromlist=f'Classes.{class_name}')
         except ModuleNotFoundError:
@@ -11,6 +21,6 @@
 
         return instance
 
-    def dynamic_create_instance_from_uri(self, class_uri):
+    def dynamic_create_instance_from_uri(self, class_uri: str) -> OTLObject | None:
         class_name = class_uri.split('#')[-1]
         return self.dynamic_create_instance_from_name(class_name)

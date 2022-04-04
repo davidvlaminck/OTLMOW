aae9b31f06b67a5cbf52d0cdec275912eade2e42
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-04-04 14:47:04 +0200

    OEF na OTL v2.3

---
+++ src/OTLMOW/OEFModel/Classes/Poortbedieningen.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from OTLMOW.OEFModel.EMObject import EMObject
+from OTLMOW.OEFModel.EMAttribuut import EMAttribuut
+from OTLMOW.OTLModel.Datatypes.StringField import StringField
+
+
+# Generated with OEFClassCreator. To modify: extend, do not edit
+class Poortbedieningen(EMObject):
+    """AUTOMATISCHE DEUREN EN POORTEN"""
+
+    typeURI = 'https://lgc.data.wegenenverkeer.be/ns/installatie#Poortbedieningen'
+    label = 'ADP'
+
+    def __init__(self):
+        super().__init__()
+
+        self._notitieinspectie = EMAttribuut(field=StringField,
+                                             naam='notitieInspectie',
+                                             label='notitieInspectie',
+                                             objectUri='https://ins.data.wegenenverkeer.be/ns/attribuut#EMObject.notitieinspectie',
+                                             definitie='Definitie nog toe te voegen voor eigenschap notitie',
+                                             owner=self)
+
+    @property
+    def notitieinspectie(self):
+        """Definitie nog toe te voegen voor eigenschap notitie"""
+        return self._notitieinspectie.waarde
+
+    @notitieinspectie.setter
+    def notitieinspectie(self, value):
+        self._notitieinspectie.set_waarde(value, owner=self)
+
--- src/OTLMOW/SampleCode/create_model.py
+++ src/OTLMOW/SampleCode/create_model.py
@@ -13,12 +13,12 @@ if __name__ == '__main__':
     # create a datamodel based on the OTL SQLite database and ttl files stored on the github
     otl_file_location = '../InputFiles/OTL 2.3.db'
     GA_file_location = '../InputFiles/Geometrie_Artefact_2.3.RC2.db'
-    otl_facility.init_otl_model_creator(otl_file_location, GA_file_location)
-    otl_facility.create_otl_datamodel()
+    # otl_facility.init_otl_model_creator(otl_file_location, GA_file_location)
+    # otl_facility.create_otl_datamodel()
 
     # create an oef model as well
     oef_file_location = '../InputFiles/oef.legacy.json'
     ins_ond_file_location = '../InputFiles/oef.ins.ond.json'
-    # otl_facility.init_oef_model_creator(oef_file_location, ins_ond_file_location)
-    # otl_facility.create_oef_datamodel()
+    otl_facility.init_oef_model_creator(oef_file_location, ins_ond_file_location)
+    otl_facility.create_oef_datamodel()
 

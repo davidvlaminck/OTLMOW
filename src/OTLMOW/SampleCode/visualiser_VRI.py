b31a1f8ab32f8375490fa04ddfa88c6c196f09e9
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-04-25 16:41:48 +0200

    visualiser_VRI

---
+++ src/OTLMOW/SampleCode/visualiser_VRI.py
@@ -0,0 +1,18 @@
+﻿from OTLMOW.Facility.OTLFacility import OTLFacility
+from OTLMOW.Loggers.ConsoleLogger import ConsoleLogger
+from OTLMOW.Loggers.TxtLogger import TxtLogger
+from OTLMOW.Loggers.LoggerCollection import LoggerCollection
+
+
+if __name__ == '__main__':
+    logger = LoggerCollection([
+        TxtLogger(r'C:\temp\pythonLogging\pythonlog.txt'),
+        ConsoleLogger()])
+    otl_facility = OTLFacility(logger)
+
+    jsonPath = "DA-2022-00191_export.json"
+
+    vri_assets = otl_facility.davieImporter.import_file(jsonPath)
+
+    otl_facility.visualiser.show(vri_assets)
+

19a966b743b0c8845c0c3f24cf78596c0b9b612b
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-04-26 15:22:19 +0200

    refactor test

--- UnitTests/FeatureRequestsTests/AgentCollection.py
+++ UnitTests/FeatureRequestsTests/AgentCollection.py
@@ -1,14 +1,17 @@
 import unittest
 
+from OTLMOW.Facility.AgentCollection import AgentCollection
+from OTLMOW.Facility.OTLFacility import OTLFacility
+from OTLMOW.Facility.RequesterFactory import RequesterFactory
 from OTLMOW.OTLModel.Classes.Agent import Agent
 
 
-@unittest.skip("feature request")
 class AgentCollectionTests(unittest.TestCase):
     def test_search_agent_by_name(self):
-        collection = AgentCollection()
-        agents_found = collection.search_by_name('geudens')
-        self.assertTrue(isinstance(agents_found, list))
-        self.assertTrue(isinstance(agents_found[0], Agent))
-        self.assertEqual('8a7c6f90-23b8-4170-b6b7-1517b8c8465b', agents_found[0].agentId[:36])
-        self.assertEqual('Dave Geudens', agents_found[0].naam)
+        otl_facility = OTLFacility(instanceLogger=None, settings_path='C:\\resources\\settings_OTLMOW.json')
+        requester = RequesterFactory.create_requester(settings=otl_facility.settings, auth_type='JWT', env='prd')
+        collection = AgentCollection(requester)
+        agents_found = collection.get_agent_by_full_name('dave geudens')
+        self.assertTrue(isinstance(agents_found, Agent))
+        self.assertEqual('8a7c6f90-23b8-4170-b6b7-1517b8c8465b', agents_found.agentId[:36])
+        self.assertEqual('Dave Geudens', agents_found.naam)

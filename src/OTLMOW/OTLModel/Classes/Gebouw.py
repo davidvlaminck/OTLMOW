9e120648221f8befce6d943a054b82b59e1ecfb7
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-05-13 11:04:44 +0200

    geometry updates

--- src/OTLMOW/OTLModel/Classes/Gebouw.py
+++ src/OTLMOW/OTLModel/Classes/Gebouw.py
@@ -2,19 +2,17 @@
 from OTLMOW.OTLModel.BaseClasses.OTLAttribuut import OTLAttribuut
 from OTLMOW.OTLModel.Classes.Behuizing import Behuizing
 from OTLMOW.OTLModel.Datatypes.DtcDocument import DtcDocument
-from OTLMOW.GeometrieArtefact.VlakGeometrie import VlakGeometrie
 
 
 # Generated with OTLClassCreator. To modify: extend, do not edit
-class Gebouw(Behuizing, VlakGeometrie):
+class Gebouw(Behuizing):
     """Elk bouwwerk, dat een voor mensen toegankelijke overdekte, geheel of gedeeltelijk met wanden omsloten ruimte vormt."""
 
     typeURI = 'https://wegenenverkeer.data.vlaanderen.be/ns/installatie#Gebouw'
     """De URI van het object volgens https://www.w3.org/2001/XMLSchema#anyURI."""
 
     def __init__(self):
-        Behuizing.__init__(self)
-        VlakGeometrie.__init__(self)
+        super().__init__()
 
         self._grondplan = OTLAttribuut(field=DtcDocument,
                                        naam='grondplan',

d6f41df1dfa26f065af1107c0fff5b9f50281fad
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-06-02 13:52:44 +0200

    OSLOCollectorTests aanpassen

--- UnitTests/GeneralTests/OSLOCollectorTests.py
+++ UnitTests/GeneralTests/OSLOCollectorTests.py
@@ -1,61 +1,29 @@
+import os
 import unittest
-from unittest.mock import Mock
 
 from OTLMOW.ModelGenerator.OSLOCollector import OSLOCollector
 from OTLMOW.ModelGenerator.OSLOInMemoryCreator import OSLOInMemoryCreator
-from GeneralTests.OSLOInMemoryCreatorTests import OSLOInMemoryCreatorTests
+from OTLMOW.ModelGenerator.SQLDbReader import SQLDbReader
 
 
 class OSLOCollectorTests(unittest.TestCase):
-    def test_mock_CollectClassesAndAttributes(self):
-        mock = Mock()
-        oSLOCreator = OSLOInMemoryCreator(mock)
-        mock.performReadQuery = OSLOInMemoryCreatorTests().mockPerformReadQuery
-        collector = OSLOCollector(oSLOCreator)
+    def test_collect(self):
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        file_location = f'{base_dir}/../OTL_AllCasesTestClass.db'
+        sql_reader = SQLDbReader(file_location)
+        oslo_creator = OSLOInMemoryCreator(sql_reader)
+        collector = OSLOCollector(oslo_creator)
         collector.collect()
 
-        # assert
-        self.assertTrue(len(collector.classes) >= 1)
         self.assertTrue(len(collector.inheritances) >= 1)
-        self.assertTrue(len(collector.primitiveDatatypes) >= 1)
-        self.assertTrue(len(collector.primitiveDatatypeAttributen) >= 1)
-        self.assertTrue(len(collector.complexDatatypes) >= 1)
-        self.assertTrue(len(collector.complexDatatypeAttributen) >= 1)
-        self.assertTrue(len(collector.unionDatatypes) >= 1)
-        self.assertTrue(len(collector.unionDatatypeAttributen) >= 1)
-        self.assertTrue(len(collector.enumerations) >= 1)
         self.assertTrue(len(collector.attributes) >= 1)
+        self.assertTrue(len(collector.classes) >= 1)
+        self.assertTrue(len(collector.complex_datatypes) >= 1)
+        self.assertTrue(len(collector.complex_datatype_attributen) >= 1)
+        self.assertTrue(len(collector.primitive_datatypes) >= 1)
+        self.assertTrue(len(collector.primitive_datatype_attributen) >= 1)
+        self.assertTrue(len(collector.union_datatypes) >= 1)
+        self.assertTrue(len(collector.union_datatype_attributen) >= 1)
+        self.assertTrue(len(collector.enumerations) >= 1)
         self.assertTrue(len(collector.relations) >= 1)
-
-
-    def test_mock_CollectThenFindAttributesByClass(self):
-        mock = Mock()
-        oSLOCreator = OSLOInMemoryCreator(mock)
-        mock.performReadQuery = OSLOInMemoryCreatorTests().mockPerformReadQuery
-        collector = OSLOCollector(oSLOCreator)
-        collector.collect()
-
-        class_uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#NaampadObject'
-        attribute_uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#NaampadObject.naampad'
-        naampadObject_class = next(c for c in collector.classes if c.objectUri == class_uri)
-        attributes = collector.find_attributes_by_class(naampadObject_class)
-
-        # assert
-        self.assertIsNotNone(naampadObject_class)
-        self.assertTrue(attributes[0].objectUri == attribute_uri)
-
-    def test_mock_CollectThenFindInheritancesByClass(self):
-        mock = Mock()
-        oSLOCreator = OSLOInMemoryCreator(mock)
-        mock.performReadQuery = OSLOInMemoryCreatorTests().mockPerformReadQuery
-        collector = OSLOCollector(oSLOCreator)
-        collector.collect()
-
-        class_uri = 'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#NaampadObject'
-        base_class = 'https://wegenenverkeer.data.vlaanderen.be/ns/implementatieelement#AIMNaamObject'
-        naampadObject_class = next(c for c in collector.classes if c.objectUri == class_uri)
-        inheritances = collector.find_inheritances_by_class(naampadObject_class)
-
-        # assert
-        self.assertIsNotNone(naampadObject_class)
-        self.assertTrue(inheritances[0].base_uri == base_class)
+        self.assertTrue(len(collector.typeLinks) >= 1)
--- src/OTLMOW/ModelGenerator/OSLOCollector.py
+++ src/OTLMOW/ModelGenerator/OSLOCollector.py
@@ -13,34 +13,34 @@ from OTLMOW.ModelGenerator.OSLOTypeLink import OSLOTypeLink
 
 
 class OSLOCollector:
-    def __init__(self, oSLOInMemoryCreator):
+    def __init__(self, oslo_in_memory_creator):
         self.inheritances = []
-        self.OSLOInMemoryCreator = oSLOInMemoryCreator
+        self.memory_creator = oslo_in_memory_creator
         self.attributes = []
         self.classes = []
-        self.primitiveDatatypes = []
-        self.primitiveDatatypeAttributen = []
-        self.complexDatatypes = []
-        self.complexDatatypeAttributen = []
-        self.unionDatatypes = []
-        self.unionDatatypeAttributen = []
+        self.primitive_datatypes = []
+        self.primitive_datatype_attributen = []
+        self.complex_datatypes = []
+        self.complex_datatype_attributen = []
+        self.union_datatypes = []
+        self.union_datatype_attributen = []
         self.enumerations = []
         self.typeLinks = []
         self.relations = [OSLORelatie]
 
     def collect(self):
-        self.classes = self.OSLOInMemoryCreator.get_all_classes()
-        self.attributes = self.OSLOInMemoryCreator.get_all_attributes()
-        self.inheritances = self.OSLOInMemoryCreator.get_all_inheritances()
-        self.primitiveDatatypes = self.OSLOInMemoryCreator.get_all_primitive_datatypes()
-        self.primitiveDatatypeAttributen = self.OSLOInMemoryCreator.get_all_primitive_datatype_attributes()
-        self.complexDatatypes = self.OSLOInMemoryCreator.get_all_complex_datatypes()
-        self.complexDatatypeAttributen = self.OSLOInMemoryCreator.get_all_complex_datatype_attributes()
-        self.unionDatatypes = self.OSLOInMemoryCreator.get_all_union_datatypes()
-        self.unionDatatypeAttributen = self.OSLOInMemoryCreator.get_all_union_datatype_attributes()
-        self.enumerations = self.OSLOInMemoryCreator.get_all_enumerations()
-        self.typeLinks = self.OSLOInMemoryCreator.get_all_typelinks()
-        self.relations = self.OSLOInMemoryCreator.get_all_relations()
+        self.classes = self.memory_creator.get_all_classes()
+        self.attributes = self.memory_creator.get_all_attributes()
+        self.inheritances = self.memory_creator.get_all_inheritances()
+        self.primitive_datatypes = self.memory_creator.get_all_primitive_datatypes()
+        self.primitive_datatype_attributen = self.memory_creator.get_all_primitive_datatype_attributes()
+        self.complex_datatypes = self.memory_creator.get_all_complex_datatypes()
+        self.complex_datatype_attributen = self.memory_creator.get_all_complex_datatype_attributes()
+        self.union_datatypes = self.memory_creator.get_all_union_datatypes()
+        self.union_datatype_attributen = self.memory_creator.get_all_union_datatype_attributes()
+        self.enumerations = self.memory_creator.get_all_enumerations()
+        self.typeLinks = self.memory_creator.get_all_typelinks()
+        self.relations = self.memory_creator.get_all_relations()
 
     def find_attributes_by_class(self, osloclass: OSLOClass) -> [OSLOAttribuut]:
         return sorted(list(filter(lambda c: c.class_uri == osloclass.objectUri, self.attributes)), key=lambda a: a.class_uri)
@@ -52,22 +52,22 @@ class OSLOCollector:
         return next(p for p in self.classes if p.objectUri == uri)
 
     def find_primitive_datatype_by_uri(self, uri: str) -> OSLODatatypePrimitive:
-        return next(p for p in self.primitiveDatatypes if p.objectUri == uri)
+        return next(p for p in self.primitive_datatypes if p.objectUri == uri)
 
     def find_primitive_datatype_attributen_by_class_uri(self, class_uri: str) -> list[OSLODatatypePrimitiveAttribuut]:
-        return sorted(list(filter(lambda p: p.class_uri == class_uri, self.primitiveDatatypeAttributen)), key=lambda p: p.class_uri)
+        return sorted(list(filter(lambda p: p.class_uri == class_uri, self.primitive_datatype_attributen)), key=lambda p: p.class_uri)
 
     def find_complex_datatype_by_uri(self, uri) -> OSLODatatypeComplex:
-        return next(p for p in self.complexDatatypes if p.objectUri == uri)
+        return next(p for p in self.complex_datatypes if p.objectUri == uri)
 
     def find_complex_datatype_attributen_by_class_uri(self, class_uri: str) -> list[OSLODatatypeComplexAttribuut]:
-        return sorted(list(filter(lambda p: p.class_uri == class_uri, self.complexDatatypeAttributen)), key=lambda p: p.class_uri)
+        return sorted(list(filter(lambda p: p.class_uri == class_uri, self.complex_datatype_attributen)), key=lambda p: p.class_uri)
 
     def find_union_datatype_by_uri(self, uri) -> OSLODatatypeUnion:
-        return next(p for p in self.unionDatatypes if p.objectUri == uri)
+        return next(p for p in self.union_datatypes if p.objectUri == uri)
 
     def find_union_datatype_attributen_by_class_uri(self, class_uri: str) -> list[OSLODatatypeUnionAttribuut]:
-        return sorted(list(filter(lambda p: p.class_uri == class_uri, self.unionDatatypeAttributen)), key=lambda p: p.class_uri)
+        return sorted(list(filter(lambda p: p.class_uri == class_uri, self.union_datatype_attributen)), key=lambda p: p.class_uri)
 
     def find_enumeration_by_uri(self, uri) -> OSLOEnumeration:
         return next(p for p in self.enumerations if p.objectUri == uri)

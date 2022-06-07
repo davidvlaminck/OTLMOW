bc88de71de8a72449f6e922ec6aeb536923aa170
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-06-07 16:54:42 +0200

    how to see the logging output

--- unittest_runner.py
+++ unittest_runner.py
@@ -1,3 +1,4 @@
+import logging
 import unittest
 
 
@@ -5,3 +6,7 @@ if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir="UnitTests", pattern="*Tests.py")
     unittest.TextTestRunner().run(suite)
+
+    # want to see info/debug messages from logging? use:
+    # logging.debug = print
+    # logging.info = print

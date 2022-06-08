0455911147066fd016722a14280bd9a30995b896
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-06-08 08:48:18 +0200

    DateTime convert to correct type implementation

--- UnitTests/OTLFieldTests/DateTimeFieldTests.py
+++ UnitTests/OTLFieldTests/DateTimeFieldTests.py
@@ -4,41 +4,40 @@ from unittest import TestCase
 
 from OTLMOW.Facility.Exceptions.CouldNotConvertToCorrectType import CouldNotConvertToCorrectType
 from OTLMOW.OTLModel.BaseClasses.OTLAttribuut import OTLAttribuut
+from OTLMOW.OTLModel.Datatypes.DateTimeField import DateTimeField
 from OTLMOW.OTLModel.Datatypes.TimeField import TimeField
 
 
 class DateTimeFieldTests(TestCase):
     def test_validate(self):
-        # TODO add tests for DateTime
-        self.assertTrue(False)
-        time_attribute = OTLAttribuut(naam='time attribuut')
-        self.assertTrue(TimeField.validate(None, time_attribute))
-        self.assertTrue(TimeField.validate(datetime.time(10, 11, 12), time_attribute))
+        datetime_attribute = OTLAttribuut(naam='datetime attribuut')
+        self.assertTrue(DateTimeField.validate(None, datetime_attribute))
+        self.assertTrue(DateTimeField.validate(datetime.datetime(2022, 2, 2, 10, 11, 12), datetime_attribute))
         with self.assertRaises(TypeError):
-            TimeField.validate('a', time_attribute)
+            TimeField.validate('a', datetime_attribute)
         with self.assertRaises(TypeError):
-            TimeField.validate('1', time_attribute)
+            TimeField.validate('1', datetime_attribute)
         with self.assertRaises(TypeError):
-            TimeField.validate([], time_attribute)
+            TimeField.validate([], datetime_attribute)
         with self.assertRaises(TypeError):
-            TimeField.validate(object(), time_attribute)
+            TimeField.validate(object(), datetime_attribute)
         with self.assertRaises(TypeError):
-            TimeField.validate(1.0, time_attribute)
+            TimeField.validate(1.0, datetime_attribute)
 
     def test_convert_to_correct_type(self):
-        # TODO add tests for DateTime
-        self.assertTrue(False)
         with self.subTest('Correct values'):
-            self.assertIsNone(TimeField.convert_to_correct_type(None))
-            self.assertEqual(datetime.time(10, 11, 12), TimeField.convert_to_correct_type(datetime.time(10, 11, 12)))
+            self.assertIsNone(DateTimeField.convert_to_correct_type(None))
+            self.assertEqual(datetime.datetime(2022, 2, 2, 10, 11, 12),
+                             DateTimeField.convert_to_correct_type(datetime.datetime(2022, 2, 2, 10, 11, 12)))
 
-        convertable_values_list = [('10:11:12', datetime.time(10, 11, 12)), ('7:8:9', datetime.time(7, 8, 9)),
-                                   ('2020-1-1 10:11:12', datetime.time(10, 11, 12)), (datetime.date(1, 1, 1), datetime.time(0, 0, 0)),
-                                   ('1/1/2020 10:11:12', datetime.time(10, 11, 12)), (10000, datetime.time(3, 46, 40))]
+        convertable_values_list = [('2020-2-2 10:11:12', datetime.datetime(2020, 2, 2, 10, 11, 12)),
+                                   ('2/2/2020 10:11:12', datetime.datetime(2020, 2, 2, 10, 11, 12)),
+                                   (datetime.date(2020, 2, 2), datetime.datetime(2020, 2, 2, 0, 0, 0)),
+                                   (10000000, datetime.datetime(1970, 4, 26, 19, 46, 40))]
         for value, expected in convertable_values_list:
             with self.subTest(f'Correct value after conversion: value = {value}'):
                 with self.assertLogs() as captured:
-                    self.assertEqual(expected, TimeField.convert_to_correct_type(value))
+                    self.assertEqual(expected, DateTimeField.convert_to_correct_type(value))
                     warnings = list(filter(lambda r: r.levelno == logging.WARNING, list(captured.records)))
                     self.assertGreater(len(warnings), 0)
 
@@ -46,4 +45,4 @@ class DateTimeFieldTests(TestCase):
         for value in incorrect_values:
             with self.subTest(f'Could not perform conversion: value = {value}'):
                 with self.assertRaises(CouldNotConvertToCorrectType):
-                    TimeField.convert_to_correct_type(value)
+                    DateTimeField.convert_to_correct_type(value)

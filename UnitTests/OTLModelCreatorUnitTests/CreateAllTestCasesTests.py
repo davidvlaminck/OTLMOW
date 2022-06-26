143ea4e5bc06ea235281c7a3eb037d6627288f67
David Vlaminck <david.vlaminck@mow.vlaanderen.be>
2022-06-26 19:52:14 +0200

    simplify unit test

--- UnitTests/OTLModelCreatorUnitTests/CreateAllTestCasesTests.py
+++ UnitTests/OTLModelCreatorUnitTests/CreateAllTestCasesTests.py
@@ -11,35 +11,20 @@ from UnitTests.TestClasses.OTLModel.Classes.Onderdeel.AllCasesTestClass import A
 
 
 class CreateAllTestCasesTests(unittest.TestCase):
-    def setUp(self) -> OSLOCollector:
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        file_location = f'{base_dir}/../OTL_AllCasesTestClass.db'
-        sql_reader = SQLDbReader(file_location)
-        oslo_creator = OSLOInMemoryCreator(sql_reader)
-        collector = OSLOCollector(oslo_creator)
-        collector.collect()
-        return collector
-
     def test_init_AllCasesTestClass(self):
         base_dir = os.path.dirname(os.path.realpath(__file__))
         settings_file_location = f'{base_dir}/../settings_OTLMOW.json'
-        otl_facility = OTLFacility(logfile='',
-                                   settings_path=settings_file_location)
+        subset_file_location = f'{base_dir}/../OTL_AllCasesTestClass.db'
 
-        collector = self.setUp()
-        otl_facility.collector = collector
-
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        otl_file_location = f'{base_dir}/../OTL_AllCasesTestClass.db'
-        #GA_file_location = '../InputFiles/Geometrie_Artefact_2.3.RC2.db'
+        otl_facility = OTLFacility(logfile='', settings_path=settings_file_location)
 
         with self.assertLogs() as captured:
-            otl_facility.create_otl_datamodel(directory=f'{base_dir}/../TestClasses', otl_sqlite_file_location=otl_file_location)
+            otl_facility.create_otl_datamodel(directory=f'{base_dir}/../TestClasses',
+                                              otl_sqlite_file_location=subset_file_location)
             allcasesclass_location = f'{base_dir}/../TestClasses/OTLModel/Classes/Onderdeel/AllCasesTestClass.py'
             self.assertTrue(os.path.isfile(allcasesclass_location))
 
         errors = list(filter(lambda r: r.levelno >= logging.ERROR, list(captured.records)))
-
         self.assertListEqual([], errors)
 
     def test_use_AllCasesTestClass(self):
